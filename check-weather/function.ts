3ba838500a6c47b143fe99dc66fa54d44f25a4cb
Dan Silver <dansil@microsoft.com>
2017-03-14 14:20:39 -0700

    removed code

--- check-weather/function.ts
+++ check-weather/function.ts
@@ -1,31 +1,8 @@
-import { OpenWeatherMapAppId } from '../secrets'
-import fetch from 'node-fetch';
-
 export function main (context, req) {
-    getWeatherForecast(req.query.city).then((weatherData) => {
-        let response = {
-            status: 200, // optional, defaults to 200
-            body: weatherData
-        };
+    let response = {
+        status: 200, // optional, defaults to 200
+        body: {sample: "Azure function works!"}
+    };
 
-        return context.done(null, response);
-    }).catch((error) => {
-
-        // try passing error as first param
-
-        context.done(null, {
-            status: 500,
-            body: {error: JSON.stringify(error)}
-        });
-    })
+    return context.done(null, response);
 };
-
-async function getWeatherForecast(cityName:string):Promise<any> {
-    if (typeof cityName === "undefined") {
-        return Promise.reject("city not passed");
-    }
-
-    let rawRes = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&mode=json&APPID=${OpenWeatherMapAppId}`);
-    let weatherObj =  await rawRes.json();
-    return weatherObj.list[0];
-}
